d99b248583a91217e4fcc536a9557a8590e5db26
Junpei Mishima <junpei.msm@gmail.com>
2020-12-31 23:30:28 +0900

    add files for CodeDeploy

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,3 @@
+pkill -KILL -f "python main.py"
+cd /home/ec2-user/discord/honda-bot-v2/
+python src/main.py &
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo rm -rf /home/ec2-user/discord/honda-bot-v2/
+sudo mkdir /home/ec2-user/discord/honda-bot-v2/
