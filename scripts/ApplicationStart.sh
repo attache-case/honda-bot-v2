2b4796fa41ff3b70242324c857e74bb5968c82b1
Junpei Mishima <junpei.msm@gmail.com>
2021-01-01 01:55:13 +0900

    update ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,4 +1,6 @@
-pkill -KILL -f "python main.py"
+#!/bin/sh
+
+# pkill -KILL -f "python main.py"
 cd /home/ec2-user/honda-bot-v2/
 source ~/py-envs/honda-bot/bin/activate
 pip install -r requirements.txt
