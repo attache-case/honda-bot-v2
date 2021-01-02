5786dbcdf9543c64d6b7afae8b2c60fe52921a30
Junpei Mishima <junpei.msm@gmail.com>
2021-01-02 14:33:11 +0900

    update files

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-# pkill -KILL -f "python main.py"
+# ssm parameter-store settings
+INSTANCE_ID=$(curl 169.254.169.254/latest/meta-data/instance-id)
+echo INSTANCE_ID=$INSTANCE_ID
+ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
+echo ZONE=$ZONE
+REGION=$(echo ${ZONE/%?/})
+echo REGION=$REGION
+APP_NAME=$(aws --region ${REGION} ec2 describe-instances --instance-ids ${INSTANCE_ID} --query "Reservations[0].Instances[0].Tags[?Key=='Name'].Value" --output text)
+echo APP_NAME=$APP_NAME
+APP_ENV=$(aws --region ${REGION} ec2 describe-instances --instance-ids ${INSTANCE_ID} --query "Reservations[0].Instances[0].Tags[?Key=='Env'].Value" --output text)
+echo APP_ENV=$APP_ENV
+
+SSM_PARAMS=$(aws --region ${REGION} ssm get-parameters-by-path --path "/${APP_NAME}/${APP_ENV}" --with-decryption)
+echo SSM_PARAMS=$SSM_PARAMS
+for param_data in $(echo $SSM_PARAMS | jq -c '.Parameters[] | { Name, Value }'); do
+    name=$(echo ${param_data} | jq -r '.Name')
+    value=$(echo ${param_data} | jq -r '.Value')
+    export ${name##*/}=${value}
+done
+
 cd /home/ec2-user/honda-bot-v2/
 source ~/py-envs/honda-bot/bin/activate
-pip3 install -r requirements.txt
-sudo nohup python3 main.py &
+pip install -r requirements.txt
+sudo nohup python main.py &
---
+++ scripts/ApplicationStop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pkill -KILL -f "python main.py"
